4d813530672c550e6d06ce7a6518b40a1f7a2629
Ul-Tim-ate <68898246+Ul-Tim-ate@users.noreply.github.com>
2020-10-18 17:48:51 +0300

    Update 1.sh

--- lab2/1.sh
+++ lab2/1.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 ps -a -u | grep -c user > task1.txt
-ps -a -u user o user,pid,command | grep user | awk '{print $2 ":" $3}' >> task1.txt 
+ps -a -u user o user,pid,command | grep user | head -n-3 |awk '{print $2 ":" $3}' >> task1.txt 
