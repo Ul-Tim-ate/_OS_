8c0cc2a1c9fdb2825274cc37c213d3a52f26b9e8
Ul-Tim-ate <68898246+Ul-Tim-ate@users.noreply.github.com>
2020-10-19 09:50:23 +0300

    Update 4.sh

--- lab2/4.sh
+++ lab2/4.sh
@@ -5,7 +5,12 @@ do
 PPid=`grep -s "PPid"  "/proc/"$pid"/status" | awk '{print $2}')`
 Runtime=`grep -s "sum_exec_runtime"  "/proc/"$pid"/sched" | awk '{print $3}')`
 Switch=`grep -s "nr_switches"  "/proc/"$pid"/sched" | awk '{print $3}')`
-#Art=$Runtime / $Switch
-done
+
+if [[ -n $Switch && -n $Runtime ]]
+then
+Art=$(bc <<< "scale=5; $Runtime/$Switch")
+echo "ProcessID=$pid : Parent_ProcessID=$PPid : Average_Running_Time=$Art"
+fi
+done | sort -n -t = -k 3 > task4.txt
 
 
