9d06f10c32490b505a8e802a889e11230cba00b3
Ul-Tim-ate <68898246+Ul-Tim-ate@users.noreply.github.com>
2020-10-19 14:21:34 +0300

    Create 6.sh

---
+++ lab2/6.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+MaxSize=0
+Pid_of_MaxSize=0
+for pid in $(ps -a -u root o pid)
+do
+Size=`grep -s "VmSize" "/proc/"$pid"/status" | awk '{print $2}'`
+if [[ $Size -gt $MaxSize ]]
+then
+MaxSize=$Size
+Pid_of_MaxSize=$pid
+fi
+done
+top -p $Pid_of_MaxSize -b -n 1 > task6.txt
+vmSize=$(cat task6.txt | grep $Pid_of_MaxSize | awk '{print $5 }')
+
