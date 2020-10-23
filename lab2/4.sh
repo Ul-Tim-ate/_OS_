2ae23b7b3dac5e00d1f8e94f76a9f1d182d58c87
Ul-Tim-ate <68898246+Ul-Tim-ate@users.noreply.github.com>
2020-10-23 10:47:58 +0300

    Update 4.sh

--- lab2/4.sh
+++ lab2/4.sh
@@ -2,9 +2,9 @@
 
 for pid in $(ps -a -u root o pid)
 do
-PPid=`grep -s "PPid"  "/proc/"$pid"/status" | awk '{print $2}')`
-Runtime=`grep -s "sum_exec_runtime"  "/proc/"$pid"/sched" | awk '{print $3}')`
-Switch=`grep -s "nr_switches"  "/proc/"$pid"/sched" | awk '{print $3}')`
+PPid=`grep -s "PPid"  "/proc/"$pid"/status" | awk '{print $2}'`
+Runtime=`grep -s "sum_exec_runtime"  "/proc/"$pid"/sched" | awk '{print $3}'`
+Switch=`grep -s "nr_switches"  "/proc/"$pid"/sched" | awk '{print $3}'`
 
 if [[ -n $Switch && -n $Runtime ]]
 then
